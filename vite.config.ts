686c188abbdfc70b2e9af0f59f1da69b4a7159bd
zyronon <zyronon@163.com>
2025-08-16 18:09:59 +0800

    fix:add git log

--- vite.config.ts
+++ vite.config.ts
@@ -22,98 +22,98 @@ export default defineConfig(() => {
     let latestCommitHash = ''
     getLastCommit((err, commit) => {
       if (!err) latestCommitHash = commit.shortHash
-    })
-    resolve({
-      plugins: [
-        VueMacros({
-          plugins: {
-            vue: Vue(),
-            vueJsx: VueJsx(), // 如果需要
-          },
-        }),
-        UnoCSS(),
-        ElementPlus(),
-        lifecycle === 'report' ?
-          visualizer({
-            gzipSize: true,
-            brotliSize: true,
-            emitFile: false,
-            filename: "report.html", //分析图生成的文件名
-            open: true //如果存在本地服务端口，将在打包后自动展示
-          }) : null,
-        SlidePlugin(),
-        importToCDN({
-          // modules: [
-          //   {
-          //     name: 'vue',
-          //     var: 'Vue',
-          //     path: `https://type-words.oss-cn-shenzhen.aliyuncs.com/vue.global.prod.min.js`
-          //   },
-          //   {
-          //     name: 'vue-router',
-          //     var: 'VueRouter',
-          //     path: `https://type-words.oss-cn-shenzhen.aliyuncs.com/vue-router.global.prod.min.js`
-          //   },
-          //   {
-          //     name: 'axios',
-          //     var: 'axios',
-          //     path: 'https://type-words.oss-cn-shenzhen.aliyuncs.com/axios.min.js'
-          //   },
-          // ]
-          modules: [
-            {
-              name: 'vue',
-              var: 'Vue',
-              path: `https://2study.top/vue.global.prod.min.js`
+      resolve({
+        plugins: [
+          VueMacros({
+            plugins: {
+              vue: Vue(),
+              vueJsx: VueJsx(), // 如果需要
             },
-            {
-              name: 'vue-router',
-              var: 'VueRouter',
-              path: `https://2study.top/vue-router.global.prod.min.js`
-            },
-            {
-              name: 'axios',
-              var: 'axios',
-              path: 'https://2study.top/axios.min.js'
-            },
-          ]
-        })
-      ],
-      define: {
-        LATEST_COMMIT_HASH: JSON.stringify(latestCommitHash + (process.env.NODE_ENV === 'production' ? '' : ' (dev)')),
-      },
-      //默认是'',导致只能在一级域名下使用。
-      base: './',
-      resolve: {
-        alias: {
-          "@": pathResolve("src"),
+          }),
+          UnoCSS(),
+          ElementPlus(),
+          lifecycle === 'report' ?
+            visualizer({
+              gzipSize: true,
+              brotliSize: true,
+              emitFile: false,
+              filename: "report.html", //分析图生成的文件名
+              open: true //如果存在本地服务端口，将在打包后自动展示
+            }) : null,
+          SlidePlugin(),
+          importToCDN({
+            // modules: [
+            //   {
+            //     name: 'vue',
+            //     var: 'Vue',
+            //     path: `https://type-words.oss-cn-shenzhen.aliyuncs.com/vue.global.prod.min.js`
+            //   },
+            //   {
+            //     name: 'vue-router',
+            //     var: 'VueRouter',
+            //     path: `https://type-words.oss-cn-shenzhen.aliyuncs.com/vue-router.global.prod.min.js`
+            //   },
+            //   {
+            //     name: 'axios',
+            //     var: 'axios',
+            //     path: 'https://type-words.oss-cn-shenzhen.aliyuncs.com/axios.min.js'
+            //   },
+            // ]
+            modules: [
+              {
+                name: 'vue',
+                var: 'Vue',
+                path: `https://2study.top/vue.global.prod.min.js`
+              },
+              {
+                name: 'vue-router',
+                var: 'VueRouter',
+                path: `https://2study.top/vue-router.global.prod.min.js`
+              },
+              {
+                name: 'axios',
+                var: 'axios',
+                path: 'https://2study.top/axios.min.js'
+              },
+            ]
+          })
+        ],
+        build: {
+          rollupOptions: {
+            external: ['axios'],// 使用全局的 axios。因为百度翻译库内部用了0.19版本的axios，会被打包到代码里面
+          }
         },
-        extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json', '.vue']
-      },
-      build: {
-        rollupOptions: {
-          external: ['axios'],// 使用全局的 axios。因为百度翻译库内部用了0.19版本的axios，会被打包到代码里面
-        }
-      },
-      css: {
-        preprocessorOptions: {
-          scss: {
-            //解决 sass 控制台出现 Deprecation Warning: The legacy JS API is deprecated and will be removed in Dart Sass 2.0.0 的问题
-            api: "modern-compiler" // or 'modern'
+        define: {
+          LATEST_COMMIT_HASH: JSON.stringify(latestCommitHash + (process.env.NODE_ENV === 'production' ? '' : ' (dev)')),
+        },
+        //默认是'',导致只能在一级域名下使用。
+        base: './',
+        resolve: {
+          alias: {
+            "@": pathResolve("src"),
+          },
+          extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json', '.vue']
+        },
+        css: {
+          preprocessorOptions: {
+            scss: {
+              //解决 sass 控制台出现 Deprecation Warning: The legacy JS API is deprecated and will be removed in Dart Sass 2.0.0 的问题
+              api: "modern-compiler" // or 'modern'
+            }
+          }
+        },
+        server: {
+          port: 3000,
+          open: false,
+          host: '0.0.0.0',
+          fs: {
+            strict: false,
+          },
+          proxy: {
+            '/baidu': 'https://api.fanyi.baidu.com/api/trans/vip/translate'
           }
         }
-      },
-      server: {
-        port: 3000,
-        open: false,
-        host: '0.0.0.0',
-        fs: {
-          strict: false,
-        },
-        proxy: {
-          '/baidu': 'https://api.fanyi.baidu.com/api/trans/vip/translate'
-        }
-      }
+      })
     })
   })
 })
