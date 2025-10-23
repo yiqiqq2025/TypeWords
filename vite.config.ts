d196ba3dc2eca6d874a18c7725b6f7b16bc5549c
yiqiqq2025 <yiqiqq2025@gmail.com>
2025-10-23 10:57:43 +0800

    Update CDN links in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -63,9 +63,9 @@ export default defineConfig(() => {
               enforce: 'pre',
               transformIndexHtml(html) {
                 const scripts = `
-<script src="https://2study.top/libs/vue.global.prod.min.js" crossorigin="anonymous"></script>
-<script src="https://2study.top/libs/vue-router.global.prod.min.js" crossorigin="anonymous"></script>
-<script src="https://2study.top/libs/axios.min.js" crossorigin="anonymous"></script>
+<script src="https://abc.yiqiqq.com/libs/vue.global.prod.min.js" crossorigin="anonymous"></script>
+<script src="https://abc.yiqiqq.com/libs/vue-router.global.prod.min.js" crossorigin="anonymous"></script>
+<script src="https://abc.yiqiqq.com/libs/axios.min.js" crossorigin="anonymous"></script>
 `
                 return html.replace('<head>', `<head>${scripts}`)
               },
